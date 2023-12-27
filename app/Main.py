11e22e26f0accaf5bfa769878710c3063e133138
Кирилл <ziabrew34@gmail.com>
2023-12-27 21:35:59 +0600

    fixed main page

--- app/Main.py
+++ app/Main.py
@@ -1,17 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-
 st.title("""
 Web-приложение для инференса  моделей ML и анализа данных
  """)
-
 
 st.header('Об авторе')
 
 st.write("""
 Зябрев Кирилл Сергеевич \n
-Cтудент группы МО-221
+cтудент группы МО-221
 """)
 
-st.image('data/images/author.jpg', caption = "2023")
+st.image('data/images/author.jpg', caption = "2023", width=200)
